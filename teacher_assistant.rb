af72106af50713ee13315c84d8dd13a29b170742
Doug Gerber <dojoh@outlook.com>
2013-09-05 16:01:46 -0400

    New CSV

--- teacher_assistant.rb
+++ teacher_assistant.rb
@@ -9,7 +9,6 @@ require 'pry'
 require 'ruby-standard-deviation'
 
 class GradeReader 
-
   def initialize
     CSV.foreach('students.csv') do |row|
     puts row.join(',')
@@ -29,13 +28,8 @@ class GradeReader
 
   def grades_for_student(row)
   	name = row.shift
-  	grades = row.collect{|i| i.to_i}
+  	grades = row.collect{ |i| i.to_i }
   	student = Student.new(name, grades)
-
-  	# student = {}
-  	# name = row.shift
-  	# student[:name] = name
-  	# student[:grade] = row
 
   	student
 	end	
@@ -43,89 +37,90 @@ end
 
 
 class GradeAverager < GradeReader
-	def initialize
-
+	def initialize(grades)
+		@grades = grades
 	end
 
-	def calculate_average(grades)
-		grades.inject{|sum, grade| sum + grade } / grades.count
+	def calculate_average
+		@grades.inject{ |sum, grade| sum + grade } / @grades.count
 	end
 end
 
 class FinalGrade < GradeReader
+	def initialize(average)
+		@average = average
+	end
 
+	def letter_grade
+		if @average >= 90
+			"A"
+		elsif @average >= 80
+			"B"
+		elsif @average >= 70
+			"C"
+		elsif @average >= 60
+			"D"
+		else
+			"F"
+		end
+	end
 end
 
 class Student < GradeReader
-	attr_accessor :name, :grades, :average
-	
 	def initialize(name, grades)
 		@name = name
 		@grades = grades
 	end
-end
 
-class GradeSummary < GradeReader
-end
-
-total_class = []
-
-grade_reader = GradeReader.new
-
-students = grade_reader.grades_for_students('students.csv')
-
-grade_averager = GradeAverager.new
-
-students.each do |student|
-	student.average = grade_averager.calculate_average(student.grades)
-	total_class << student.average
-	puts "#{student.name}: #{student.average}"
-
-	if student.average >= 90
-		puts "#{student.name}: A"
-	elsif student.average >= 80 && student.average < 90
-		puts "#{student.name}: B"
-	elsif student.average >= 70 && student.average < 80
-		puts "#{student.name}: C"
-	elsif student.average >= 60 && student.average < 70
-		puts "#{student.name}: D"
-	elsif student.average < 60
-		puts "#{student.name}: F"
+	def stats
+		averager = GradeAverager.new(@grades)
+		grader = FinalGrade.new(averager.calculate_average)
+	 	[@name, grader.letter_grade, averager.calculate_average]
 	end
 end
 
-# max_grade = []
-# min_grade =[]
-# standard_deviation = []
+class GradeSummary < GradeReader
+	def initialize
+		@students = grades_for_students('students.csv')
+	end
 
-
-# max_grade = total_class.max.round
-# min_grade = total_class.min.round
-# standard_deviation = total_class.stdev.round
-
-#it was looking for an array instead of a string, so I put them into arrays
-
-CSV.open("final_grades.csv", "w") do |final_grades|
-  final_grades << total_class
-  final_grades << [total_class.max]
-  final_grades << [total_class.min]
-  final_grades << [total_class.stdev]
+	def write_summary(file_name)
+		CSV.open(file_name, "w") do |csv|
+			@students.each do |student|
+				csv << student.stats
+			end
+		end
+	end
 end
 
 
+puts "File bro?"
+input = gets.chomp
 
-# Save into an array
-# The average score across the class is outputted
-# The minimum score across the class is outputted
-# The maximum score across the class is outputted
-# The standard deviation across the class is outputted
+summarizer = GradeSummary.new
+summarizer.write_summary(input)
 
-# The file should be sorted by last name, first name
-# The file should contain the average score (rounded to the first decimal) and the letter grade for each student
 
-# Acceptance Criteria: 
-# * I receive an error if the file does not exist 
-# * I receive an error if the file is not the intended extension 
-# * I receive an error if an entry doesn't have the same number of grades as the others 
-# * If the file is valid, it provides the source data that runs the program
 
+# total_class = []
+
+# grade_reader = GradeReader.new
+
+# students = grade_reader.grades_for_students('students.csv')
+
+# grade_averager = GradeAverager.new
+
+# students.each do |student|
+# 	student.average = grade_averager.calculate_average(student.grades)
+# 	total_class << student.average
+# 	puts "#{student.name}: #{student.average}"
+# end
+
+
+
+# CSV.open("final_grades.csv", "w") do |final_grades|
+#   final_grades << total_class
+#   final_grades << [total_class.max]
+#   final_grades << [total_class.min]
+#   final_grades << [total_class.stdev]
+# end
